446a885201404043f604e03a8d23baa5b087f976
Adam Clifton <akclifto@asu.edu>
2019-11-23 13:56:43 -0700

    test works

--- src/test/java/EarthTest.java
+++ src/test/java/EarthTest.java
@@ -1,15 +1,36 @@
 import main.factory.Earth;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class EarthTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @Test
     public void Earth_test_instance() {
 
-        
+
         Earth e = new Earth();
         e.getEarth();
         String s = outContent.toString().trim();
