df8dc8eb8de9a0071695af8699257fd02b1dc2b9
Adam Clifton <akclifto@asu.edu>
2019-11-24 21:55:47 -0700

    test factory

--- src/test/java/factory/HotSpotTest.java
+++ src/test/java/factory/HotSpotTest.java
@@ -2,12 +2,26 @@ package factory;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class HotSpotTest {
 
+    private HotSpot lair = new HotSpot("testSpot");
+
+
     @Test
     public void addLair() {
+
+        VillainLair newLair = new VillainLair();
+        lair.addLair(newLair);
+        List<LairBase> lairs = lair.getHotspots();
+
+        assertEquals(newLair, lairs.get(0));
+
+
     }
 
     @Test
