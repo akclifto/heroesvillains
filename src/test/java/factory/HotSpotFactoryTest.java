d90e2a3acd3b56bc03f371cad495d997954671f8
Adam <aclifton01@hotmail.com>
2019-11-25 21:03:02 -0700

    test hotspotfactory

--- src/test/java/factory/HotSpotFactoryTest.java
+++ src/test/java/factory/HotSpotFactoryTest.java
@@ -6,16 +6,34 @@ import static org.junit.Assert.*;
 
 public class HotSpotFactoryTest {
 
+    HotSpotFactory hotspot;
+
     @Test
     public void getLairs() {
+
+        hotspot = new HotSpotFactory();
+        hotspot.initHotspot("TestCity");
+        hotspot.createLair();
+        hotspot.getLairs();
+        assertEquals(1, hotspot.getLairs().size());
     }
 
     @Test
     public void getBases() {
+
+        hotspot = new HotSpotFactory();
+        hotspot.initHotspot("TestCity");
+        hotspot.createBase();
+        hotspot.getBases();
+        assertEquals(1, hotspot.getBases().size());
+
     }
 
     @Test
     public void initHotspot() {
+
+        
+
     }
 
     @Test
